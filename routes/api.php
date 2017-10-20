d61d009e65dd5b77e6a4970a5e5ffab6fda52a1e
Chr15t1an <ccampbell@sapphirebd.com>
2017-10-20 13:43:32 -0400

    Testing Payload.

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,32 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+
+Route::post('/integrations/shopify/', function () {
+
+    return  response()->json(['success' => 'success'], 200);
+
+
+    
+  #Capture Request Object.
+  $request = Request();
+
+  #Captue Payload | Array
+  $payload = $request->all();
+
+
+
+
+  // return gettype($payload);
+
+  // $payload_string = json_encode($payload);
+  // $payload_object = json_decode($payload_string);
+
+//  return "<pre>".print_r($payload)."</pre>";
+
+  return  response()->json(['success' => 'success'], 200);
 });
